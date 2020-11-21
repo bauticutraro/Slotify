32455115ccebf9f7e019e1471329b73524e4c16f
bauticutraro <bautistacutraro@gmail.com>
2020-11-21 18:48:57 -0300

    delete console log

--- src/containers/Auth/Login.jsx
+++ src/containers/Auth/Login.jsx
@@ -13,8 +13,6 @@ const Login = () => {
       hashParams[e[1]] = decodeURIComponent(e[2]);
     }
 
-    console.log(process.env, 'hola');
-
     if (!hashParams.access_token) {
       window.location.href = `https://accounts.spotify.com/authorize?client_id=10387b6d5e8b4da7b09ac055d680fa9e&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=${process.env.REACT_APP_SERVER}`;
     } else {
