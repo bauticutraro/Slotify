679a1533a9f7f2517c1b93e4d8cf775820cc7406
bauticutraro <bautistacutraro@gmail.com>
2020-11-21 17:19:24 -0300

    .

--- src/containers/Auth/Login.jsx
+++ src/containers/Auth/Login.jsx
@@ -13,6 +13,8 @@ const Login = () => {
       hashParams[e[1]] = decodeURIComponent(e[2]);
     }
 
+    console.log(process.env, 'hola');
+
     if (!hashParams.access_token) {
       window.location.href = `https://accounts.spotify.com/authorize?client_id=06e092ef90ba49e380c9349cc4302613&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=${process.env.REACT_APP_SERVER}`;
     } else {
